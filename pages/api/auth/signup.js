24b928e42c147074f0cf9300c4b64374113bba9e
mj305 <maria.j.beckles@gmail.com>
2020-11-17 18:46:32 -0500

    cleaned the code in signup.js

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -24,28 +24,3 @@ const handler = (req, res) => {
 };
 
 export default handler;
-
-/*   try {
-    const result = await axios.post(
-      "https://api.bigcommerce.com/stores/m48amdwp20/v3/customers",
-      {
-        data: req.body.data,
-      },
-      {
-        headers: {
-          "Content-Type": "application/json",
-          "X-Auth-Client": "4b90lbc8d76whmbmtttdxx6zqyhpohw",
-          "X-Auth-Token": "ckuij76tgurkmapz1gspgbihq7doxd9",
-        },
-      }
-    );
-    console.log(result);
-
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "application/json");
-    res.end(JSON.stringify({ name: "SUCCESS" }));
-  } catch (error) {
-    console.log("Error: ", error);
-    res.end(JSON.stringify({ name: "ERROR" }));
-  } 
-  */
